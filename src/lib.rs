1715997343c1f14080bfd1c315a83227523eb959
llgoer <llgoer@github.com>
2019-10-22 15:23:53 +0800

    增加一个简单的版本

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+// 这里编写一个两个数相加的函数
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
